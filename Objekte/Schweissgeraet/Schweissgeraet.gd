8689d467c07dd0bc0acca0b135faa7675bd05ba5
HSA xRLab PC5 <xrlab05@hs-anhalt.de>
2024-06-05 14:07:16 +0200

    Schweißgerät GUI Zeugs

---
+++ Objekte/Schweissgeraet/Schweissgeraet.gd
@@ -0,0 +1,26 @@
+extends Node3D
+class_name Schweissmaschine
+
+var ui:Control
+var display:Label 
+var strom_ein:bool:
+	set(neu_ein):
+		strom_ein = neu_ein
+		display.visible = strom_ein
+
+var strom:int = 90:
+	set(neuer_strom):
+		strom = neuer_strom
+		display.text = str(strom)+" A"
+		
+var spannung:float = 0
+var abstand:float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ui = $Bildschirm.scene_node
+	assert(ui!=null,"UI konnte nicht importiert werden.")
+	display = ui.find_child("Stromanzeige")
+	display.text = str(ui.find_child("Stromslider").value)+" A"
+	
+	strom_ein = false
---
+++ Objekte/Schweissgeraet/Schweissmaschine_UI.gd
@@ -0,0 +1,23 @@
+extends Control
+var schweissmaschine:Schweissmaschine
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	schweissmaschine = find_parent("Schweissgeraet")
+	assert(schweissmaschine!=null,"UI konnte Schweißmaschine nicht finden. Wurde die richtige Szene ausgeführt?")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_v_slider_value_changed(value):
+	schweissmaschine.strom = value
+	
+
+
+func _on_einschalter_toggled(toggled_on):
+	schweissmaschine.strom_ein = toggled_on
