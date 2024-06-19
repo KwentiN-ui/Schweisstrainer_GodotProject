f27e6e849e5caab2b45d395c85d95d7cd3ca2197
HSA xRLab PC5 <xrlab05@hs-anhalt.de>
2024-06-19 13:56:21 +0200

    Raupe Nimmersatt

--- Objekte/Schweissgeraet/RayCast3D_test.gd
+++ Objekte/Schweissgeraet/RayCast3D_test.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 const max_abstand = 1
+signal nahtpunkt_erzeugen(pos:Vector3,normale:Vector3)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
