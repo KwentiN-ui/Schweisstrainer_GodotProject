2d1ee2bde7f2116ed76df793cc8b8b2993c97f60
sydgtfzguhgfhtr <schulzn2004@gmail.com>
2024-06-22 10:12:25 +0200

    Aufseher schwebt nicht mehr und bleibt in begrenztem Raum in dem Spieler ist

--- Objekte/Aufseher.gd
+++ Objekte/Aufseher.gd
@@ -6,12 +6,10 @@ extends Node3D
 var movement_delta: float
 var animation: AnimationPlayer
 var spieler: Node3D
-var alter_winkel_spieler: float
 var alte_pos_spieler: Vector3
 
 func _ready() -> void:
 	spieler = find_parent("Main").find_child("XRCamera3D")
-	alter_winkel_spieler = spieler.global_rotation.y
 	alte_pos_spieler = spieler.global_position
 	animation = find_child("AnimationPlayer")
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
@@ -23,11 +21,23 @@ func abstand():
 	return Vector3(-abstand_zum_spieler*sin(spieler.global_rotation.y),0,-abstand_zum_spieler *cos(spieler.global_rotation.y))
 
 func _physics_process(delta):
+	print(spieler.global_position.x, "-", spieler.global_position.z)
+	var ziel = spieler.global_position+abstand()
+	if spieler.global_position.x >= 0.4 and spieler.global_position.z <= 5 and spieler.global_position.z >= -9.3:
+		if ziel.x <= 0.4:
+			ziel.x = 1
+		if ziel.x >= 10:
+			ziel.x = 10
+		if ziel.z  >= 5.1:
+			ziel.z = 5.1
+		if ziel.z <= -9:
+			ziel.z = -9
 	var v_spieler_aufseher: Vector3 = spieler.global_position - global_position
 	var abstand: float = sqrt(v_spieler_aufseher.x**2 + v_spieler_aufseher.y**2 + v_spieler_aufseher.z**2)
 	var bewegung = sqrt((spieler.global_position.x-alte_pos_spieler.x)**2 + (spieler.global_position.y-alte_pos_spieler.y)**2 + (spieler.global_position.z-alte_pos_spieler.z)**2 )
-	if abs(alter_winkel_spieler - spieler.global_rotation.y) > PI or bewegung > 1:
-		set_movement_target(spieler.global_position+abstand())
+	if bewegung > 1:
+		set_movement_target(ziel)
+		position.y=0
 	if navigation_agent.is_navigation_finished():
 		look_at(spieler.global_position)
 		rotation.x = 0
@@ -47,7 +57,6 @@ func _physics_process(delta):
 		navigation_agent.velocity = new_velocity
 	else:
 		_on_velocity_computed(new_velocity)
-	alter_winkel_spieler = spieler.global_rotation.y
 
 func _on_velocity_computed(safe_velocity: Vector3) -> void:
 	global_position = global_position.move_toward(global_position + safe_velocity, movement_delta)
