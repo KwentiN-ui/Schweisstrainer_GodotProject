43f7645858e9939965c2c24aa22773c12f83cbcd
HSA xRLab PC5 <xrlab05@hs-anhalt.de>
2024-06-26 11:30:06 +0200

    ShapeCast3D

--- Objekte/Schweissgeraet/RayCast3D_test.gd
+++ Objekte/Schweissgeraet/RayCast3D_test.gd
@@ -1,21 +1,25 @@
-extends RayCast3D
+extends ShapeCast3D
 
 const max_abstand = 1
 signal nahtpunkt_erzeugen(pos:Vector3,normale:Vector3)
+var Kollisionsobjekte: Array[Object]
+
+var schweissgeraet: Schweissmaschine
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	schweissgeraet = find_parent("Schweissgeraet")
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _physics_process(delta):
-	#var space_state = get_world_3d().direct_space_state
-	#var origin = global_position
-	#var richtung = global_transform.basis * Vector3(max_abstand,0,0)
-	#var ziel = origin + richtung
-	#$zielmesh.global_position = ziel
-	#
-	#var query = PhysicsRayQueryParameters3D.create(origin, ziel)
-	#var result = space_state.intersect_ray(query)
-	#print(result.values())
+func _physics_process(delta):
+	shape.height = schweissgeraet.abstand
+	shape.radius = schweissgeraet.elektrodendurchmesser/2
+	for i in get_collision_count():
+		Kollisionsobjekte.push_back(get_collider(i))
+	print(Kollisionsobjekte)
+	Kollisionsobjekte.clear()
+
+
--- Objekte/Schweissgeraet/Schweissgeraet.gd
+++ Objekte/Schweissgeraet/Schweissgeraet.gd
@@ -3,6 +3,8 @@ class_name Schweissmaschine
 
 @export var halter:XRToolsPickable
 
+var csg_poly: CSGPolygon3D
+
 var ui:Control
 var display:Label 
 var strom_ein:bool:
@@ -18,11 +20,26 @@ var strom:int = 90:
 var spannung:float = 0
 var abstand:float = 0
 
+var elektrodendurchmesser: float
+
+var elektroden_laenge: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	csg_poly = find_child("CSGPolygon3D")
 	ui = $Bildschirm.scene_node
 	assert(ui!=null,"UI konnte nicht importiert werden.")
 	display = ui.find_child("Stromanzeige")
 	display.text = str(ui.find_child("Stromslider").value)+" A"
 	
 	strom_ein = false
+
+func _physics_process(delta):
+	elektrodenform()
+
+func elektrodenform():
+	var w = deg_to_rad(360/25)
+	var punkte: PackedVector2Array
+	for i in range(0,25):
+		punkte.append(Vector2(cos(w*(i+1))*elektrodendurchmesser, sin(w*(i+1))*elektrodendurchmesser))
+	csg_poly.polygon = punkte
