5e34c1c51d9a61923cc99d1273364c4047c2161a
KwentiN <quentinhuss@hotmail.com>
2024-07-02 22:46:47 +0200

    Elektrodenquerschnitt neu eingebaut, funktioniert noch nicht so richtig

--- Objekte/Schweissgeraet/Schweissgeraet.gd
+++ Objekte/Schweissgeraet/Schweissgeraet.gd
@@ -6,6 +6,8 @@ func _ready():
 	Schweisslogik.ui = $Bildschirm.scene_node
 	Schweisslogik.halter["root"] = $Elektrodenhalter
 	Schweisslogik.halter["path3d"] = $Elektrodenhalter/Path3D
+	Schweisslogik.halter["querschnitt"] = $Elektrodenhalter/querschnitt
+	Schweisslogik.elektrode_d = 0.04
 
 	Schweisslogik.stromdisplay = Schweisslogik.ui.find_child("Stromanzeige")
 	Schweisslogik.stromdisplay.text = str(Schweisslogik.ui.find_child("Stromslider").value)+" A"
--- Singletons/Schweisslogik.gd
+++ Singletons/Schweisslogik.gd
@@ -6,9 +6,11 @@ extends Node
 var schweissmaschine: Schweissmaschine
 
 var halter = {
-	"root":null,
-	"path3d":null,
+	"root":null,   # XRToolsPickable
+	"path3d":null, # Path3D Node
+	"querschnitt":null, # CSGPolygon3
 }
+
 var t = 0.0
 var stromdisplay: Label
 var ui:Control
@@ -23,9 +25,10 @@ var strom:int = 0:
 		if is_instance_valid(stromdisplay):
 			stromdisplay.text = str(strom)+" A"
 
-var elektrode_d = 0.04: #m Durchmesser
+var elektrode_d: #m Durchmesser, Startwert wird in der Schweißmaschine festgelegt
 	set(neu):
 		elektrode_d = neu
+		refresh_elektrodenquerschnitt()
 
 var elektrode_l = 0.3: #m Länge
 	set(neu):
@@ -37,7 +40,7 @@ func _ready():
 
 func _process(delta):
 	t += delta
-	elektrode_l = (sin(t)+1)/2 * 0.4
+	elektrode_l = (sin(t)+1)/2 * elektrode_l
 
 func refresh_elektrodenpfad():
 	if is_instance_valid(halter["path3d"]) and is_instance_valid(halter["path3d"]):
@@ -45,9 +48,15 @@ func refresh_elektrodenpfad():
 		var curve:Curve3D = pfad.curve
 		curve.set_point_position(1,Vector3(elektrode_l,0,0))
 
+func refresh_elektrodenquerschnitt():
+	if is_instance_valid(halter["querschnitt"]):
+		var qs:CSGPolygon3D = halter["querschnitt"]
+		qs.polygon = kreisform_koordinaten(elektrode_d/2.0)
+
+
 func kreisform_koordinaten(radius:float)->PackedVector2Array:
-	var w = deg_to_rad(360/15.0)
-	var punkte: PackedVector2Array
-	for i in range(0,15):
-		punkte.append(Vector2(cos(w*(i+1))*radius, sin(w*(i+1))*radius))
+	var punkte = []
+	for i in range(0,floor(2*PI*100),20):
+		var w = i/100.0
+		punkte.append(Vector2(radius*cos(w),radius*sin(w)))
 	return punkte
